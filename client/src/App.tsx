2996b48b74eebb21326c305e8a1766c502c0ef15
arvindh07 <arvindhganesh11@gmail.com>
2024-07-12 19:10:50 +0530

    done initial routing based on user status

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,31 @@
+import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import Home from "./app_components/Home";
+import Login from "./app_components/Login";
+import { useState } from "react";
+
+
 function App() {
+  const [userLoggedIn, _] = useState<boolean>(false);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: userLoggedIn ? <Navigate to="/" /> : <Navigate to="/login" />,
+      children: [
+        {
+          index: true,
+          element: <Home />
+        },
+        {
+          path: "login",
+          element: <Login />
+        }
+      ]
+    },
+  ])
+
   return (
-    <h1>Hello World!!!</h1>
+    <RouterProvider router={router} />
   )
 }
 
