6b8676eb97f55496fb2014ffb6ee8d0da806af36
arvindh07 <arvindhganesh11@gmail.com>
2024-07-13 09:18:31 +0530

    redirect basic done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,24 @@
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./app_components/Home";
-import Login from "./app_components/Login";
+import Auth from "./app_components/Auth";
 import { useState } from "react";
-
+import Redirect from "./app_components/Redirect";
 
 function App() {
-  const [userLoggedIn, _] = useState<boolean>(false);
+  const [userLoggedIn, _] = useState<boolean>(true);
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: userLoggedIn ? <Navigate to="/" /> : <Navigate to="/login" />,
+      element: <Redirect user={userLoggedIn} />,
       children: [
         {
           index: true,
           element: <Home />
         },
         {
-          path: "login",
-          element: <Login />
+          path: "auth",
+          element: <Auth />
         }
       ]
     },
---
+++ client/src/app_components/Redirect.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+
+interface RedirectProps {
+    user: boolean
+}
+const Redirect = ({user}: RedirectProps) => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(!user){
+            navigate("/auth")
+        } else {
+            navigate("/")
+        }
+    }, [])
+
+    return <><Outlet /></>
+}
+
+export default Redirect
