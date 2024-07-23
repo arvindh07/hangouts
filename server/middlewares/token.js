5ab74660fbc6c0b0a730cf9e071f0750298217e8
arvindh07 <arvindhganesh11@gmail.com>
2024-07-23 20:36:13 +0530

    changed token mins to hr

--- server/middlewares/token.js
+++ server/middlewares/token.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const createToken = (userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "30s"
+        expiresIn: "1h"
     })
     return token;
 }
