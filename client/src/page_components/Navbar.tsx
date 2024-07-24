0565b9c438b443d059f25a66086eda93694db595
arvindh07 <arvindhganesh11@gmail.com>
2024-07-24 18:53:07 +0530

    added logout logic

--- client/src/page_components/Navbar.tsx
+++ client/src/page_components/Navbar.tsx
@@ -1,7 +1,21 @@
+import { useDispatch } from "react-redux"
+import { appActions } from "../store/slices/rootSlice";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(appActions?.setLogin(false));
+    localStorage?.removeItem("user");
+    navigate("/");
+  }
+
   return (
-    <div>
-        <div className="bg-slate-100 text-xl px-6 my-3 p-3">Vinisha</div>
+    <div className="bg-slate-100 flex justify-between items-center px-6 my-3 p-3">
+      <div className="text-xl">Vinisha</div>
+      <button className="text-gray-600 hover:text-black" onClick={handleLogout}>Logout</button>
     </div>
   )
 }
