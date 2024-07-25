c310d2b618c831cea157fc3b70476594f962f824
arvindh07 <arvindhganesh11@gmail.com>
2024-07-25 19:54:53 +0530

    added scrollbar for messages section

--- client/src/page_components/MessageSection.tsx
+++ client/src/page_components/MessageSection.tsx
@@ -1,17 +1,27 @@
 import { useState } from "react"
 
 const MessageSection = () => {
-    const [messages, setMessages] = useState<string[]>([])
+    const [messages, setMessages] = useState<any[]>([
+        { content: "Hi", sender: "Me", chatRoom: "MeVinisha" },
+        { content: "Hello", sender: "Vinu", chatRoom: "MeVinisha" },
+        { content: "Hw r u?", sender: "Me", chatRoom: "MeVinisha" },
+        { content: "I am fine. How abt u?", sender: "Vinu", chatRoom: "MeVinisha" },
+        { content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, possimus consectetur consequuntur eos dolorum facere tempora praesentium quod natus, laborum odit quisquam debitis reiciendis repellat.", sender: "Vinu", chatRoom: "MeVinisha" },
+        { content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, possimus consectetur consequuntur eos dolorum facere tempora praesentium quod natus, laborum odit quisquam debitis reiciendis repellat.", sender: "Vinu", chatRoom: "MeVinisha" },
+        { content: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Libero, possimus consectetur consequuntur eos dolorum facere tempora praesentium quod natus, laborum odit quisquam debitis reiciendis repellat.", sender: "Vinu", chatRoom: "MeVinisha" },
+    ])
+    const currentUser = "Me";
 
     return (
-        <div className="h-4/5">
-            {messages?.map((msg: any, index: number) => {
+        <div className="h-4/5 overflow-y-auto message">
+            {messages?.map((msgObj: any, index: number) => {
                 return (
-                    <div className={`flex flex-col`}>
+                    <div className={`flex flex-col px-2 w-full`}>
                         <div
                             key={index}
-                            className={`inline-block bg-blue-100 mb-2 rounded-md p-2 px-4`}>
-                            {msg}
+                            className={`max-w-[60%] inline-block bg-blue-100 mb-2 rounded-md p-2 px-4 
+                            ${currentUser === msgObj?.sender ? "ml-auto" : "mr-auto"} text-wrap`}>
+                            {msgObj?.content}
                         </div>
                     </div>
                 )
