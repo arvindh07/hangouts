08afc82e14bd10178091553861b2cce5850ef1fc
arvindh07 <arvindhganesh11@gmail.com>
2024-07-27 10:35:06 +0530

    removed socket in chat page

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,22 +1,10 @@
-import { useEffect } from "react";
 import Navbar from "../page_components/Navbar"
 import ChatSection from "./ChatSection"
-import io from "socket.io-client";
-
 interface ChatProps {
     currentChat: string;
 }
 
-export let socket: any;
-
 const Chat = ({ currentChat }: ChatProps) => {
-    useEffect(() => {
-        socket = io("http://localhost:6999");
-        socket.on("connect", () => {
-            console.log("Socket connected in client");
-        })
-    }, [])
-
     return (
         <>
             <Navbar currentChat={currentChat} />
