225739dc847ece1f1cdef85dd7528b4c2b9ac00c
arvindh07 <arvindhganesh11@gmail.com>
2024-07-27 20:07:44 +0530

    return all chats sorted and added empty validation

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -2,6 +2,11 @@ import Chat from "../models/chatSchema.js";
 
 export const handleAccessOrCreateChat = async (req, res, next) => {
     const { userId } = req.body;
+    if(!userId){
+        return res.status(400).json({
+            msg: "Id required"
+        })
+    }
     // search the inc user in chat.users
     // if found: return the chat
     // else: create chat
@@ -20,15 +25,17 @@ export const handleAccessOrCreateChat = async (req, res, next) => {
         let chat = await Chat.create({
             users: [userId, req.user]
         });
-        chat = chat.populate("users");
+        chat = await chat.populate("users", "username email profilePic");
         return res.status(201).json(chat);
     }
     
-    return res.status(200).json(chatFound);
+    return res.status(200).json(chatFound[0]);
 }
 
 export const handleGetAllChats = async (req, res, next) => {
     // get all chats that logged in user is part of
-    const chats = await Chat.find({ users: req.user }).populate("users", "username email profilePic");
+    const chats = await Chat.find({ users: req.user })
+        .populate("users", "username email profilePic")
+        .sort({'updatedAt': -1});
     return res.status(200).json(chats);
 }
