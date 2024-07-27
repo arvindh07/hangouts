c8cd8f357043cf4183135564a407833e7901f48a
arvindh07 <arvindhganesh11@gmail.com>
2024-07-27 10:32:29 +0530

    add setUser method in rootslice

--- client/src/store/slices/rootSlice.ts
+++ client/src/store/slices/rootSlice.ts
@@ -1,13 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+interface StateInterface {
+    isLoggedIn: boolean;
+    user: {
+        username: string;
+        id: string;
+        email: string;
+        profilePic: string;
+        token: string;
+    }
+}
+const initialState: StateInterface = {
+    isLoggedIn: false,
+    user: {
+        username: "",
+        id: "",
+        email: "",
+        profilePic: "",
+        token: ""
+    }
+}
+
 const rootSlice = createSlice({
     name: "app",
-    initialState: {
-        isLoggedIn: false
-    },
+    initialState,
     reducers: {
         setLogin: (state, action) => {
             state.isLoggedIn = action.payload;
+        },
+        setUser: (state, action) => {
+            state.user = action.payload;
         }
     }
 })
