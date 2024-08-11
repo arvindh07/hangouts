8a65f09890223897c2026e852017eaca8fd628f5
arvindh07 <arvindhganesh11@gmail.com>
2024-08-11 19:10:05 +0530

    logout route added

--- client/src/api/paths.ts
+++ client/src/api/paths.ts
@@ -1,3 +1,4 @@
 export const LOGIN_ROUTE = "/user/login";
 export const REGISTER_ROUTE = "/user/register";
 export const REFRESH_ROUTE = "/user/refresh";
+export const LOGOUT_ROUTE = "/user/logout";
