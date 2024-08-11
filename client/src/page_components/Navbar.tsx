0e3a8002f2a87ea0dfb124787d37d1c9247449ad
arvindh07 <arvindhganesh11@gmail.com>
2024-08-11 19:12:44 +0530

    removed logout in chat section

--- client/src/page_components/Navbar.tsx
+++ client/src/page_components/Navbar.tsx
@@ -1,24 +1,11 @@
-import { useDispatch } from "react-redux"
-import { appActions } from "../store/slices/rootSlice";
-import { useNavigate } from "react-router-dom";
-
 interface NavbarProps {
   currentChat: string;
 }
+
 const Navbar = ({currentChat}: NavbarProps) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleLogout = () => {
-    dispatch(appActions?.setLogin(false));
-    localStorage?.removeItem("user");
-    navigate("/");
-  }
-
   return (
     <div className="bg-slate-100 flex justify-between items-center px-6 my-3 p-3 rounded-sm">
       <div className="text-xl">{currentChat}</div>
-      <button className="text-gray-600 hover:text-black" onClick={handleLogout}>Logout</button>
     </div>
   )
 }
