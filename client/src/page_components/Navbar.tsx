31433570768d49c12d5aee8b6beb87d8657696b1
arvindh07 <arvindhganesh11@gmail.com>
2024-08-25 15:07:46 +0530

    css for sticky top

--- client/src/page_components/Navbar.tsx
+++ client/src/page_components/Navbar.tsx
@@ -1,12 +1,17 @@
+import { IoMdArrowRoundBack } from "react-icons/io";
 import { capitalizeWords } from "../utils/common";
 
 interface NavbarProps {
   currentChat: any;
+  setChatListScreen: any;
 }
 
-const Navbar = ({ currentChat }: NavbarProps) => {
+const Navbar = ({ currentChat, setChatListScreen }: NavbarProps) => {
   return (
-    <div className="bg-slate-200 flex items-center px-6 p-3 rounded-sm h-1/6 max-h-[60px]">
+    <div className="bg-slate-400 flex items-center px-6 py-3 sticky top-0">
+      <div className="text-white md:hidden mr-2">
+        <IoMdArrowRoundBack onClick={() => setChatListScreen((prev: any) => !prev)} />
+      </div>
       <img src={currentChat?.profilePic} alt="" className="w-7 h-7 rounded-full object-cover" />
       <div className="text-lg ml-5">{capitalizeWords(currentChat?.username)}</div>
     </div>
